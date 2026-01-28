7297f829b94aaec189c8de8896327288c46a8743
Nuclear-alt <jhgutierrez@ualr.edu>
2026-01-28 00:20:28 +0000

    Frist assignment

---
+++ 01.Project.py
@@ -0,0 +1,14 @@
+years = 365
+Days = 24
+lengthoftime = years // Days
+print(lengthoftime)
+
+days1 =
+ =
+lengthoftime =  // 
+print(lengthoftime)
+
+ =
+ =
+lengthoftime =  // 
+print(lengthoftime)
