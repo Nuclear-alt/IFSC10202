b5e36f655e41018bbf7ad9b9eddaec103f937254
Nuclear-alt <jhgutierrez@ualr.edu>
2026-01-28 01:20:01 +0000

    Frist assignment

--- 01.Project.py
+++ 01.Project.py
@@ -3,12 +3,21 @@ Days = 24
 lengthoftime = years // Days
 print(lengthoftime)
 
-days1 =
- =
-lengthoftime =  // 
-print(lengthoftime)
+days1 = 24
+hours = 60
+lengthoftime1 = days1 // hours
+print(lengthoftime1)
 
- =
- =
-lengthoftime =  // 
-print(lengthoftime)
+hours1 = 60
+minute = 60
+lengthoftime2 = hours1 // minute
+print(lengthoftime2)
+
+seconds = lengthoftime + lengthoftime1 + lengthoftime2
+print(seconds)
+
+minute1 = 60
+seconds = 16.47789855072464
+lengthoftime3 = minute1 // seconds
+print(lengthoftime3)
+
