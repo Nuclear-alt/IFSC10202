2db05bbc6e7d0bbe5b5287b6f4641ea4ceea44b8
Nuclear-alt <jhgutierrez@ualr.edu>
2026-02-02 16:16:02 +0000

    3 Content and Exercises

---
+++ 03.02 Equal Numbers.py
@@ -0,0 +1,9 @@
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+c = int(input("Enter Third Number: "))
+if a == b and c == c:
+    print(3)
+elif a == b or b == c or a == c:
+    print(2)
+else:
+    print(0)
