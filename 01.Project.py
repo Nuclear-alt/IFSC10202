bca6dec567cb3a37208ceebdea845a12f8da0801
Nuclear-alt <jhgutierrez@ualr.edu>
2026-02-04 06:08:24 +0000

    01 Project assignment

--- 01.Project.py
+++ 01.Project.py
@@ -1,11 +1,11 @@
 Seconds = 50000001
-years = 365 * 24 * 60 * 60
+years = 365
 years = Seconds // years
 
-days = 24 * 60 * 60
+days = 24
 days = Seconds // days
 
-hours = 60 * 60
+hours = 60
 hours = Seconds // hours
 
 minutes = 60
