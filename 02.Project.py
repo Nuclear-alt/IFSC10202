09d6e3320ed7799965e814d13ba1d4e85b48f903
Nuclear-alt <jhgutierrez@ualr.edu>
2026-02-05 23:14:27 +0000

    02 Project assignment

--- 02.Project.py
+++ 02.Project.py
@@ -1,10 +1,10 @@
 from math import pi, sin, cos, acos
 EnterRadiusofSphere = 6371
 
-S1 = StartingPoint = 52
-E1 = EnterLatitude = 21
-S2 = StartingPoint = 41
-E2 = EnterLatitude = 12
+S1 = Latitude = 52
+S2 = Latitude = 41
+E1 = Longitude = 21
+E2 = Longitude = 12
 
 S1 = S1 * pi / 180
 E1 = E1 * pi / 180
