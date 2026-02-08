cd61a6a320ef62f83a75d26dacbff68675c0cf49
Nuclear-alt <jhgutierrez@ualr.edu>
2026-02-08 00:29:18 +0000

    03 Project assignment

--- 03.Project.py
+++ 03.Project.py
@@ -1,5 +1,15 @@
 First_Number = float(input("Enter First Number: "))
-Operator = int(input("Enter Operator (+,-.*,/): "))
+Enter_Operator = input("Enter Operator (+, -, *, /): ")
 Second_Number = float(input("Enter Second Number: "))
 
-if First_Number
+if Enter_Operator == "+":
+    print(First_Number + Second_Number)
+elif Enter_Operator == "-":
+    print(First_Number - Second_Number)
+elif Enter_Operator == "*":
+    print(First_Number * Second_Number)
+elif Enter_Operator == "/":
+    if Second_Number != 0:
+        print(First_Number / Second_Number)
+else:
+    print("Invalid Operator")
