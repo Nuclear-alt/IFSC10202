e83ab2cb56aab67de7eec6bb185da28ace6d5cb8
Nuclear-alt <jhgutierrez@ualr.edu>
2026-02-09 16:35:06 +0000

    02 Project assignment

--- 02.Project.py
+++ 02.Project.py
@@ -2,8 +2,8 @@ from math import pi, sin, cos, acos
 EnterRadiusofSphere = 6371
 
 S1 = Latitude = 52
-S2 = Latitude = 41
 E1 = Longitude = 21
+S2 = Latitude = 41
 E2 = Longitude = 12
 
 S1 = S1 * pi / 180
@@ -15,4 +15,8 @@ d = EnterRadiusofSphere * acos(
     sin(E1) * sin(E2) +
     cos(E1) * cos(E2) * cos(S1 - S2))
 d = round(d, 2)
+print("latitude", S1)
+print("longitude", E1)
+print("latitude", S2)
+print("longitude", E2)
 print("The Great Circle Distance is:", d)
