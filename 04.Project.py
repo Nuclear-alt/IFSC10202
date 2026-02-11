1f3dfeae487d574739e7903654596f1c4f668174
Nuclear-alt <jhgutierrez@ualr.edu>
2026-02-11 16:46:57 +0000

    04 Project assignment

--- 04.Project.py
+++ 04.Project.py
@@ -1,2 +1,5 @@
-H = int(input("Enter maximum height 8: "))
-for i in range()
+H = int(input("Enter maximum height: "))
+for i in range(1, H + 1):
+    for r in range(1, i + 1):
+        print(r, end='')
+    print()
