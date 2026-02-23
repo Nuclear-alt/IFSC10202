e0c2e9390064d21290e31171eb54440f966dab64
Nuclear-alt <jhgutierrez@ualr.edu>
2026-02-23 16:29:47 +0000

    Chap 6

---
+++ 06.01 Circle.py
@@ -0,0 +1,5 @@
+from math import pi
+def diameter(r):
+    return 2 * r
+print("{: >15s} {: >15s} {: >15s} {: >15s}".format("Radius", "Diamter", "Circumferece", "Area"))
+inputfile = open("06.01 Radius.txt")
