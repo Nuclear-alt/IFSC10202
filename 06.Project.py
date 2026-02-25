b93a89e441ccd7e761f9637757270db9165c8b71
Nuclear-alt <jhgutierrez@ualr.edu>
2026-02-25 16:41:20 +0000

    06 Project assignment

--- 06.Project.py
+++ 06.Project.py
@@ -1,1 +1,12 @@
-Newfile = open("06.Project Output File.txt")
+First_file  = "06.Project Output File.txt"
+Second_file = "06.Project Input File.txt"
+third_file  = "06.Project Merge File.txt"
+
+input_count = 0
+merge_count = 0
+output_count = 0
+
+with open(First_file, "w") as output_file:
+    with open(Second_file, "r") as input_file:
+        for line in input_file:
+            input_count += 1
