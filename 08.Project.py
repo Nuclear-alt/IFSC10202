992bc3cbb1f53f574debfbedc5eec04f704b67c2
Nuclear-alt <jhgutierrez@ualr.edu>
2026-03-09 15:50:33 +0000

    08 Project assignment

---
+++ 08.Project.py
@@ -0,0 +1,6 @@
+US_Constitution = int(input("Enter search termliquor liquor:"))
+
+input_file = open("https://brucebauer.info/constitution.txt.")
+output_file = open("https://brucebauer.info/constitution.txt.")
+
+print(line = 273)
