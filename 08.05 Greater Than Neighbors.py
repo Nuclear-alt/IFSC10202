e77e00d15ec1c7f5ed2cf328f111ceb27feaebf0
Nuclear-alt <jhgutierrez@ualr.edu>
2026-03-09 15:12:19 +0000

    Chap 8

---
+++ 08.05 Greater Than Neighbors.py
@@ -0,0 +1,8 @@
+x = input("Enter Values Separated by Spaces: ")
+count = 0
+a = x.split()
+
+for i in range(1, len(a) - 1):
+    if int(a[i]) > int(a[i + 1]) and int(a[i]) > int(a[i - 1]):
+        count += 1
+print(count)
