9b4150dfdf64cd88e1d0a3708a315394fe576751
Nuclear-alt <jhgutierrez@ualr.edu>
2026-03-12 20:57:49 +0000

    08 Project assignment

--- 08.Project.py
+++ 08.Project.py
@@ -1,6 +1,36 @@
-US_Constitution = int(input("Enter search termliquor liquor:"))
+lines = []
 
-input_file = open("https://brucebauer.info/constitution.txt.")
-output_file = open("https://brucebauer.info/constitution.txt.")
+file = open("constitution.txt", "r")
 
-print(line = 273)
+for line in file:
+    line = line.rstrip()
+    lines.append(line)
+file.close()
+
+while True:
+    term = input("Enter search term: ")
+    if term == "":
+        break
+    i = 0
+
+    while i < len(lines):
+
+        if term.lower() in lines[i].lower():
+
+            start = i
+            while start > 0 and lines[start] != "":
+                start = start - 1
+
+            end = i
+            while end < len(lines) and lines[end] != "":
+                end = end + 1
+
+            j = start
+            while j < end:
+                print("Line", j + 1, ":", lines[j])
+                j = j + 1
+            print()
+
+            i = end
+        else:
+            i = i + 1
