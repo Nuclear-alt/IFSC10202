cb05651850c7790590fd2623171cc9a2e69afa8b
Nuclear-alt <jhgutierrez@ualr.edu>
2026-03-16 14:27:54 +0000

    8 Project assignment

--- 08.Project.py
+++ 08.Project.py
@@ -1,16 +1,14 @@
 lines = []
 
-file = open("constitution.txt", "r")
-
-for line in file:
-    line = line.rstrip()
-    lines.append(line)
-file.close()
+with open("constitution.txt", "r") as file:
+    for line in file:
+        lines.append(line.rstrip())
 
 while True:
     term = input("Enter search term: ")
     if term == "":
         break
+
     i = 0
 
     while i < len(lines):
@@ -19,18 +17,19 @@ while True:
 
             start = i
             while start > 0 and lines[start] != "":
-                start = start - 1
+                start -= 1
 
             end = i
             while end < len(lines) and lines[end] != "":
-                end = end + 1
+                end += 1
 
             j = start
             while j < end:
                 print("Line", j + 1, ":", lines[j])
-                j = j + 1
-            print()
+                j += 1
 
+            print()
             i = end
+
         else:
-            i = i + 1
+            i += 1
